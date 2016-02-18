b55bcb3cae95e3119d53d98932b47d396545583e
Cyril Leroux <Cyril@macbook-pro-de-cyril-1.home>
2016-02-18 14:11:18 +0100

    First commit

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,15 +1,14 @@
 class TodoItem
-  # include Listable
+  include Listable
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
+
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_description
-    "#{@description}".ljust(25)
-  end
+
   def format_date
     @due ? @due.strftime("%D") : "No due date"
   end
@@ -21,7 +20,7 @@ class TodoItem
     return value
   end
   def details
-    format_description + "due: " +
+    format_description((@description) + "due: " +
     format_date +
     format_priority
   end
