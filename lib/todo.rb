4d986df2efc710f62322dbb375d2bf26a6f15c04
cendrars59 <cyr.leroux@outlook.com>
2016-02-18 22:53:43 +0100

    listable file fxing

--- lib/todo.rb
+++ lib/todo.rb
@@ -3,7 +3,6 @@ class TodoItem
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
-
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
@@ -12,6 +11,7 @@ class TodoItem
   def format_date
     @due ? @due.strftime("%D") : "No due date"
   end
+
   def format_priority
     value = " ⇧" if @priority == "high"
     value = " ⇨" if @priority == "medium"
@@ -19,9 +19,11 @@ class TodoItem
     value = "" if !@priority
     return value
   end
+
   def details
-    format_description((@description) + "due: " +
+    format_description(@description) + "due: " +
     format_date +
     format_priority
   end
+
 end
